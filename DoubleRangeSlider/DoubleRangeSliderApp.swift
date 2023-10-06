5a5a895aac01fc55a1ac5bed114581c8dbd44ec9
Elaidzha1940 <eli080293@gmail.com>
2023-10-07 01:48:37 +0300

    DoubleRangeSlider

---
+++ DoubleRangeSlider/DoubleRangeSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleRangeSliderApp.swift
+//  DoubleRangeSlider
+//
+//  Created by Elaidzha Shchukin on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DoubleRangeSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
