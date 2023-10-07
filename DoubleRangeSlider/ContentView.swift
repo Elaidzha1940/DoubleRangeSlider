09a8f01c559549a55bd3936c3a5ad73a85d4312d
Elaidzha1940 <eli080293@gmail.com>
2023-10-07 03:04:52 +0300

    Updated GeometryReader

--- DoubleRangeSlider/ContentView.swift
+++ DoubleRangeSlider/ContentView.swift
@@ -1,21 +1,50 @@
-// 
-//  ContentView.swift
-//  DoubleRangeSlider
+//  /*
 //
-//  Created by Elaidzha Shchukin on 07.10.2023.
+//  Project: DoubleRangeSlider
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.10.2023
 //
+//  */
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var width: CGFloat = 0
+    @State var widthTow: CGFloat = 15
+    
+    let offsetValue: CGFloat = 40
+    
+    @State var totalScreen: CGFloat = 0
+    
+    let maxValue: CGFloat = 1000
+    
+    @State var isLeft = false
+    @State var isRight = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            GeometryReader(content: { geometry in
+                VStack {
+                    Text("0 - 1000")
+                        .foregroundStyle(.white)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.white)
+                            .opacity(0.3)
+                            .frame(height: 5)
+                        
+                    }
+                }
+                .frame(width: geometry.size.width, height: 130)
+            })
+            .frame(height: 130)
         }
-        .padding()
     }
 }
 
--- DoubleRangeSlider/DoubleRangeSliderApp.swift
+++ DoubleRangeSlider/DoubleRangeSliderApp.swift
@@ -1,9 +1,11 @@
+//  /*
 //
-//  DoubleRangeSliderApp.swift
-//  DoubleRangeSlider
+//  Project: DoubleRangeSliderApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.10.2023
 //
-//  Created by Elaidzha Shchukin on 07.10.2023.
-//
+//  */
 
 import SwiftUI
 
